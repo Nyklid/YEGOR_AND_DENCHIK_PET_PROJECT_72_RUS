8ed7360570b71a8e53ce9079b8c942e029fe4a4c
Nyklid <danilpas12@gmail.com>
2025-10-05 17:01:12 +0500

    Add files via upload

---
+++ CheckInfo.cs
@@ -0,0 +1,40 @@
+﻿namespace calc
+{
+    internal class CheckInfo
+    {
+        private int gavno { get; set; }
+        private int mocha { get; set; }
+
+
+        public CheckInfo(int gavno, int mocha)
+        {
+            this.gavno = gavno;
+            this.mocha = mocha;
+        }
+
+        public int addition(int gavno, int mocha)
+        {
+            return gavno + mocha;
+        }
+
+        public int multiply(int gavno, int mocha)
+        {
+            return gavno * mocha;
+        }
+
+        public int divide(int gavno, int mocha)
+        {
+            return gavno / mocha;
+        }
+
+        public int modulo(int gavno, int mocha)
+        {
+            return gavno % mocha;
+        }
+
+        public int subtraction(int gavno, int mocha)
+        {
+            return gavno - mocha;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string disciprtion = ("Хуйня из под коня соси жопу еблан ебучий сучий уебок сын хуйни пошел ты нахуй парашник ебливый уебок");
+            int gavno = Convert.ToInt32(Console.ReadLine());
+            int mocha = Convert.ToInt32(Console.ReadLine());
+            string kall = Console.ReadLine();
+
+            CheckInfo checkInfo = new CheckInfo(gavno, mocha);
+
+            switch (kall)
+            {
+                case "+":
+                    Console.WriteLine($"Слажения: {checkInfo.addition(gavno, mocha)}");
+                    break;
+
+                case "*":
+                    Console.WriteLine($"Умнажения: {checkInfo.multiply(gavno, mocha)}");
+                    break;
+
+                case "/":
+                    Console.WriteLine($"Деления: {checkInfo.divide(gavno, mocha)}");
+                    break;
+
+                case "%":
+                    Console.WriteLine($"Астатак с писи: {checkInfo.modulo(gavno, mocha)}");
+                    break;
+
+                case "-":
+                    Console.WriteLine($"Дешовое вычитос: {checkInfo.subtraction(gavno, mocha)}");
+                    break;
+
+                default:
+                    Console.WriteLine($"{disciprtion}");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
